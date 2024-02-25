44bd4981d2f07cb36bf34e99d7beae94b5055424
hykim <hiyoung.kimmy@gmail.com>
2024-02-24 19:41:30 -0800

    Backend : next-auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,65 @@
+import { bcrypt } from 'bcryptjs';
+import dbConnect from "@/backend/config/dbConnect";
+import User from "@/backend/models/users";
+import IUser from "@/backend/models/users";
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from 'next-auth';
+import CredentailsProvider from 'next-auth/providers/credentials';
+
+
+type Credentials = {
+  email: string;
+  password: string;
+};
+
+// type : 일반적인 타입을 구현할때 사용되며
+// interface : 객체의 타입을 구현할때 사용된다.
+
+async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, {
+    session: {
+      strategy: "jwt",
+    },
+    providers: [
+      CredentailsProvider({
+        // @ts-igonore
+        async authorize(credentials: Credentials) {
+          dbConnect();
+
+          const { email, password } = credentials;
+          const user = await User.findOne({ email }).select("+password");
+          
+          if (!user) {
+            throw new Error("Invalid Email or Password");
+          }
+
+          const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+          if(!isPasswordMatched) {
+            throw new Error("Invalid Email or Password");
+          }
+
+          return user;
+        },
+      })
+    ],
+    callbacks: {
+      jwt: async (token, user) => {
+        console.log('token=>',token);
+
+        user && (token.user = user);
+        return token;
+      },
+      session: async (session, token) => {
+        session.user = token.user as IUser;
+
+        console.log('session=>', session);
+        return session;
+      },
+    secret: process.env.NEXTAUTH_SECRET
+    }
+  });
+}
+
+export { auth as GET, auth as POST };
+
--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ const nextConfig = {
   env: {
     API_URL: "http://localhost:3000",
     DB_LOCAL_URI : "mongodb+srv://kimmy:rootroot@cluster0.curlpbd.mongodb.net/",
-    DB_URI : ""
+    DB_URI : "",
+    NEXTAUTH_SECRET: "SDFSDFSDFS",
+    NEXTAUTH_URL: "http://localhost:3000",
   },
   images: {
     domains: ['res.cloudinary.com'],
